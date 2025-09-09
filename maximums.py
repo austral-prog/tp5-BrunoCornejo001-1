658460772d19dae21f39750e43e61e5522bf4c51
BrunoCornejo001 <bcornejo@mail.austral.edu.ar>
2025-09-09 10:49:27 -0300

    Update maximums.py

--- maximums.py
+++ maximums.py
@@ -1,6 +1,4 @@
 def max_of_two(x, y):
-    x = int(input("Enter a number:"))
-    y = int(input("Enter another number:"))
     if x>y:
         return x
     else:
@@ -8,9 +6,6 @@ def max_of_two(x, y):
 
 
 def max_of_three(x, y, z):
-    x = int(input("Enter a number:"))
-    y = int(input("Enter another number:"))
-    z = int(input("Enter one more number:"))
     if x>y and x>z:
         return x
     elif y>x and y>z:
